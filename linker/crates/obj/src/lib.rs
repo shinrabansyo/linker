2e1946f6341658850dd81008dc8a042ee1ed06a1
NakagamiYuta <guguru0014@gmail.com>
2025-05-22 19:58:51 +0900

    [fix] Object::dump での書き込み後に flush を実行するようにした

--- linker/crates/obj/src/lib.rs
+++ linker/crates/obj/src/lib.rs
@@ -17,12 +17,13 @@ impl Object {
         Object { name, code }
     }
 
-    pub fn dump(&self, f: &mut impl Write) -> anyhow::Result<()> {
+    pub fn dump(&self, mut f: &mut impl Write) -> anyhow::Result<()> {
         // Header
         f.write_all(&[0x12, 0x04])?;
 
         // Body
-        serde_cbor::to_writer(f, &self)?;
+        serde_cbor::to_writer(&mut f, &self)?;
+        f.flush()?;
 
         Ok(())
     }
