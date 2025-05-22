e1f261c087c3a103d5f592bcd844f2abda0f26fd
NakagamiYuta <guguru0014@gmail.com>
2025-05-22 19:20:25 +0900

    [add] Object をファイルに出力できるようにした

--- linker/crates/obj/src/lib.rs
+++ linker/crates/obj/src/lib.rs
@@ -1,5 +1,9 @@
 pub mod inst;
 
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
 use serde::{Serialize, Deserialize};
 
 use inst::Inst;
@@ -13,5 +17,17 @@ pub struct Object {
 impl Object {
     pub fn new(name: String, code: Vec<Inst>) -> Object {
         Object { name, code }
+    }
+
+    pub fn dump<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        let mut f = File::create(path)?;
+
+        // Header
+        f.write_all(&[0x12, 0x04])?;
+
+        // Body
+        serde_cbor::to_writer(&mut f, &self)?;
+
+        Ok(())
     }
 }
