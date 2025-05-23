f94eba109f21668513a05a408c31d0aa57bff191
NakagamiYuta <guguru0014@gmail.com>
2025-05-23 16:15:16 +0900

    [add] 依存クレートに sb_asm を追加

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -1,13 +1,18 @@
 use std::io::{Read, Seek};
 
+use sb_asm::assemble_ir;
 use sb_linker_load::load;
 
-pub fn link<R: Read + Seek>(inputs: Vec<&mut R>) -> anyhow::Result<()> {
+pub fn link<R: Read + Seek>(inputs: Vec<&mut R>) -> anyhow::Result<String> {
     // 1. リンク対象ファイルを読み込む
     let _ = inputs
         .into_iter()
         .map(load)
         .collect::<anyhow::Result<Vec<_>>>()?;
 
-    Ok(())
+    // n. アセンブル
+    let (data, inst) = assemble_ir(vec![], vec![])?;
+    let asm = format!("{}\n===\n{}", data, inst);
+
+    Ok(asm)
 }
