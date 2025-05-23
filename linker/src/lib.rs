737ce69e2d0c4ee93d6854b06faf4f2e136dd9df
NakagamiYuta <guguru0014@gmail.com>
2025-05-23 16:16:50 +0900

    [udpate] link 関数の引数に Config を追加

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -1,9 +1,10 @@
 use std::io::{Read, Seek};
 
 use sb_asm::assemble_ir;
+use sb_linker_config::Config;
 use sb_linker_load::load;
 
-pub fn link<R: Read + Seek>(inputs: Vec<&mut R>) -> anyhow::Result<String> {
+pub fn link<R: Read + Seek>(_: Config, inputs: Vec<&mut R>) -> anyhow::Result<String> {
     // 1. リンク対象ファイルを読み込む
     let _ = inputs
         .into_iter()
