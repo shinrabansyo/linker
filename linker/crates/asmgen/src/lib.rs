1d22b0323872ae99a9daaf19e567642bbeca1c1d
NakagamiYuta <guguru0014@gmail.com>
2025-05-23 20:45:03 +0900

    [change] asmgen が 1 つの完全なアセンブリコードを返すようにした

--- linker/crates/asmgen/src/lib.rs
+++ linker/crates/asmgen/src/lib.rs
@@ -5,8 +5,9 @@ use sb_linker_obj::Object;
 
 use inst::asmgen_inst;
 
-pub fn asmgen(config: &Config, objs: Vec<Object>) -> anyhow::Result<(String, String)> {
+pub fn asmgen(config: &Config, objs: Vec<Object>) -> anyhow::Result<String> {
     let asm_data = "".to_string();
     let asm_inst = asmgen_inst(config, objs)?;
-    Ok((asm_data, asm_inst))
+    let asm = format!("{}\n===\n{}", asm_data, asm_inst);
+    Ok(asm)
 }
