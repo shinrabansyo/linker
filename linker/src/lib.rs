8c8e36586ee220900ac56d3798163da56384c84a
NakagamiYuta <guguru0014@gmail.com>
2025-05-23 18:46:36 +0900

    [change] link 関数を link_files と link_objs に分解

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -2,24 +2,30 @@ use std::io::{Read, Seek};
 
 use sb_asm::assemble_ir;
 use sb_linker_config::Config;
+use sb_linker_obj::Object;
 use sb_linker_load::load;
 use sb_linker_layout::layout;
 use sb_linker_asmgen::asmgen;
 
-pub fn link<R: Read + Seek>(_: Config, inputs: Vec<&mut R>) -> anyhow::Result<String> {
+pub fn link_files<R: Read + Seek>(config: Config, inputs: Vec<&mut R>) -> anyhow::Result<String> {
     // 1. リンク対象ファイルの読み込み
     let objs = inputs
         .into_iter()
         .map(load)
         .collect::<anyhow::Result<Vec<_>>>()?;
 
-    // 2. データ配置決定
+    // 2. リンク
+    link_objs(config, objs)
+}
+
+pub fn link_objs(_: Config, objs: Vec<Object>) -> anyhow::Result<String> {
+    // 1. データ配置決定
     let objs = layout(objs);
 
-    // 3. アセンブリコード生成
+    // 2. アセンブリコード生成
     let (asm_data, asm_inst) = asmgen(objs);
 
-    // 4. アセンブル
+    // 3. アセンブル
     let (data, inst) = assemble_ir(asm_data, asm_inst)?;
     let asm = format!("{}\n===\n{}", data, inst);
 
