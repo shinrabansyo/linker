7c40f84fe9cbb152a0db550406c7757c73ce7446
NakagamiYuta <guguru0014@gmail.com>
2025-05-23 20:36:13 +0900

    [change] link_files, link_objs を link に統合

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -1,27 +1,21 @@
 use std::io::{Read, Seek};
 
 use sb_linker_config::Config;
-use sb_linker_obj::Object;
 use sb_linker_load::load;
 use sb_linker_layout::layout;
 use sb_linker_asmgen::asmgen;
 
-pub fn link_files<R: Read + Seek>(config: Config, inputs: Vec<&mut R>) -> anyhow::Result<(String, String)> {
+pub fn link<R: Read + Seek>(config: Config, inputs: Vec<&mut R>) -> anyhow::Result<(String, String)> {
     // 1. リンク対象ファイルの読み込み
     let objs = inputs
         .into_iter()
         .map(load)
         .collect::<anyhow::Result<Vec<_>>>()?;
 
-    // 2. リンク
-    link_objs(config, objs)
-}
-
-pub fn link_objs(config: Config, objs: Vec<Object>) -> anyhow::Result<(String, String)> {
-    // 1. データ配置決定
+    // 2. データ配置決定
     let objs = layout(objs);
 
-    // 2. アセンブリコード生成
+    // 3. アセンブリコード生成
     let asm = asmgen(&config, objs)?;
 
     Ok(asm)
