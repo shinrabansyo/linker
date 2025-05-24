248c35840e26806fecd1ab476cb9012fd54e4cbb
NakagamiYuta <guguru0014@gmail.com>
2025-05-24 19:51:19 +0900

    [fix] link 関数の引数の型を修正

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -8,7 +8,7 @@ use sb_linker_load::load;
 use sb_linker_layout::layout;
 use sb_linker_asmgen::asmgen;
 
-pub fn link<R: Read + Seek>(config: Config, inputs: Vec<&mut R>) -> anyhow::Result<String> {
+pub fn link<R: Read + Seek>(config: Config, inputs: Vec<R>) -> anyhow::Result<String> {
     // 1. リンク対象ファイルの読み込み
     let objs = inputs
         .into_iter()
