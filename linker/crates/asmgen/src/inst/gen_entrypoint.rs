65283f37b2707d83b6d48382cfdd5ffca5240db9
NakagamiYuta <guguru0014@gmail.com>
2025-05-24 20:11:54 +0900

    [fix] エントリポイントでのスタックポインタのセットアップミスを修正

--- linker/crates/asmgen/src/inst/gen_entrypoint.rs
+++ linker/crates/asmgen/src/inst/gen_entrypoint.rs
@@ -5,7 +5,7 @@ use sb_linker_obj::{inst, Object};
 pub fn gen_entrypoint(config: &Config) -> Object {
     Object::new("__entrypoint".to_string(), vec![
         // スタックポインタ調整
-        inst!(Addi 0, 0, InstValue::Imm(config.general.stack_addr as i32)),
+        inst!(Addi 2, 0, InstValue::Imm(config.general.stack_addr as i32)),
 
         // main 関数呼び出し
         inst!(Beq 1, 0, 0, InstValue::Function("global.main".to_string())),
