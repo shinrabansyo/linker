f1dcbb39903c32468009aea17817dd96892a6475
NakagamiYuta <guguru0014@gmail.com>
2025-06-28 18:31:26 +0900

    [add] 実行時に設定ファイルを指定できるようにした

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ use sb_linker::link;
 
 fn main() -> anyhow::Result<()> {
     let args = std::env::args();
-    if args.len() < 3 {
-        return Err(anyhow::anyhow!("usage: sb-linker <input> <output>"));
+    if args.len() < 4 {
+        return Err(anyhow::anyhow!("usage: sb-linker <config> <input> <output>"));
     }
 
     let path = env::args().nth(1).unwrap();
-    let asm = link(Config::default(), vec![File::open(&path)?])?;
+    let config = toml::from_str::<Config>(&std::fs::read_to_string(&path)?)?;
 
     let path = env::args().nth(2).unwrap();
+    let asm = link(config, vec![File::open(&path)?])?;
+
+    let path = env::args().nth(3).unwrap();
     let mut f = File::create(&path)?;
     f.write_all(asm.as_bytes())?;
 
